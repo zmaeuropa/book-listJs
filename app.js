40387ac33231842d2b4c8ac39373fbed006ed493
zmaeuropa <smain.rabhi@gmail.com>
2020-06-30 18:22:05 +0200

    add validation and alert

--- app.js
+++ app.js
@@ -23,6 +23,25 @@ UI.prototype.addBookToList = function(book) {
 
     list.appendChild(row);
 }
+//Show alert
+UI.prototype.showAlert = function (message, className) {
+    //Create div
+    const div = document.createElement('div');
+    //Add classe
+    div.className = `alert ${className}`;
+    //Add text
+    div.appendChild(document.createTextNode(message));
+    //Get parent
+    const container = document.querySelector('.container');
+    //Get form
+    const form = document.querySelector('#book-form');
+    //Insert alert
+    container.insertBefore(div,form);
+    // Timeout after 3 seconds
+    setTimeout(function() {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
 
 //Clear input fields
 UI.prototype.clearFields = function() {
@@ -45,12 +64,24 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
 
     // Instantiate UI
     const ui = new UI();
-    
-    //Add book to list
-    ui.addBookToList(book);
+    // Validate
+    if (title === '' || author === '' || isbn === '') {
+        //Error alert
+        ui.showAlert('Please fill in all fields', 'error');
 
-    //Clear input fields
-    ui.clearFields();
+    } else {
+        //Add book to list
+        ui.addBookToList(book);
+
+          //Clear input fields
+        ui.clearFields();
+
+        //Show alert succes
+        ui.showAlert('Book Added', 'success');
+    }
+   
+
+  
 
     e.preventDefault();
 });
